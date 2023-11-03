681776fe65a96cc97b6aceb3a6a077ba246b3201
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 09:41:41 +0100

    lol

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,11 +3,18 @@ import analy from "../assets/971.jpg";
 
 const Analytics = () => {
   return (
-    <div className="w-full bg-white py-16 px-4 text-black">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto flex flex-col md:grid-cols-2 md:grid">
         <img src={analy} alt="laptop" />
         <div>
-          <p>data analytics dashboard</p>
+          <p className="text-[#00DF9a] uppercase">data analytics dashboard</p>
+          <h1>Manage Data Analytics Centrally</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit et
+            voluptatum iusto asperiores reprehenderit obcaecati quibusdam
+            voluptates earum autem possimus blanditiis, sapiente unde nulla
+            minima sequi! Aliquid id ipsa sapiente!
+          </p>
         </div>
       </div>
     </div>
