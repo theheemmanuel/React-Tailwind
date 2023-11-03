4e516a9eb03a3cb47d78de05b4a4361f50112389
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 14:24:55 +0100

    added footer

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import Frame1 from "../assets/Frame (2).png";
-// import Frame2 from "../assets/Frame (3).png";
-// import Frame3 from "../assets/Frame (5).png";
+import Frame2 from "../assets/Frame (3).png";
+import Frame3 from "../assets/Frame (5).png";
 
 const Cards = () => {
   return (
@@ -11,6 +11,40 @@ const Cards = () => {
           <img
             className="w-12 mx-auto mt-[-3rem] bg-white"
             src={Frame1}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-6">Single User</h2>
+          <p className="text-center font-bold text-4xl">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8 border-t">500GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+          </div>
+          <button className="mx-auto bg-[#00DF9a] w-[200px] rounded-md font-medium my-6 py-3 text-black">
+            Start Trial
+          </button>
+        </div>
+        <div className="bg-gray-100 w-full shadow-xl flex flex-col md:my-0 my-4 p-4 rounded-lg hover:scale-105 duration-500">
+          <img
+            className="w-12 mx-auto mt-[-3rem]"
+            src={Frame2}
+            alt=""
+          />
+          <h2 className="text-2xl font-bold text-center py-6">Single User</h2>
+          <p className="text-center font-bold text-4xl">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8 border-t">500GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+          </div>
+          <button className="mx-auto bg-black w-[200px] rounded-md font-medium my-6 py-3 text-[#00DF9a]">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col my-4 p-4 rounded-lg hover:scale-105 duration-500">
+          <img
+            className="w-12 mx-auto mt-[-3rem] bg-white"
+            src={Frame3}
             alt=""
           />
           <h2 className="text-2xl font-bold text-center py-6">Single User</h2>
