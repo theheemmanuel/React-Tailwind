44f7bda1f0fff3a507c93b22b3f9673878adcd0b
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 14:17:15 +0100

    cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -14,13 +14,15 @@ const Cards = () => {
             alt=""
           />
           <h2 className="text-2xl font-bold text-center py-6">Single User</h2>
-          <p>$149</p>
-          <div className="">
-            <p>500GB Storage</p>
-            <p>1 Granted User</p>
-            <p>Send up to 2 GB</p>
+          <p className="text-center font-bold text-4xl">$149</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8 border-t">500GB Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
           </div>
-          <button>Start Trial</button>
+          <button className="mx-auto bg-[#00DF9a] w-[200px] rounded-md font-medium my-6 py-3 text-black">
+            Start Trial
+          </button>
         </div>
       </div>
     </div>
