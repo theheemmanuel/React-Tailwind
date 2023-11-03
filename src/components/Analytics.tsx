517df2246c6da2d64897117b28793e52ebe32be3
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 09:56:40 +0100

    newsletter

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -4,17 +4,24 @@ import analy from "../assets/971.jpg";
 const Analytics = () => {
   return (
     <div className="w-full bg-white py-16 px-4">
-      <div className="max-w-[1240px] mx-auto flex flex-col md:grid-cols-2 md:grid">
-        <img src={analy} alt="laptop" />
-        <div>
-          <p className="text-[#00DF9a] uppercase">data analytics dashboard</p>
-          <h1>Manage Data Analytics Centrally</h1>
-          <p>
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img className="w-[500px] mx-auto my-4" src={analy} alt="laptop" />
+        <div className="flex flex-col justify-center mx-2 max-md:text-center">
+          <p className="text-[#00DF9a] uppercase font-bold">
+            data analytics dashboard
+          </p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl text-black font-bold py-2">
+            Manage Data Analytics Centrally
+          </h1>
+          <p className="text-black">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit et
             voluptatum iusto asperiores reprehenderit obcaecati quibusdam
             voluptates earum autem possimus blanditiis, sapiente unde nulla
             minima sequi! Aliquid id ipsa sapiente!
           </p>
+          <button className="mx-auto bg-black w-[200px] rounded-md font-medium my-6 py-3 text-[#00DF9a] md:mx-0">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
