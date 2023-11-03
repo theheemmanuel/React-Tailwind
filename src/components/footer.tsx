cecd811af7d80dc1ed920887dfe0d36a23479272
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 16:07:37 +0100

    footer icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,29 @@
-// import React from 'react'
-// import {
-//   FaDribbleSquare,
-//   FaFacebookSquare,
-//   FaGithubSquare,
-//   FaInstagram,
-//   FaTwitterSquare,
-// } from "react-icons/fa";
+// import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 const footer = () => {
   return (
     <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
       <div>
         <h1 className="text-3xl font-bold text-[#00DF9a]">HeeFinance.</h1>
+        <p className="py-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+          praesentium libero explicabo asperiores earum obcaecati eligendi
+          repellendus nam repellat similique.
+        </p>
+        <div className="flex md:w-[75%] justify-between">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
+        </div>
       </div>
     </div>
   );
