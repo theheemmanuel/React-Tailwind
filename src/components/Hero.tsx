fae1c7740c721ed46faf78fc79bfeb351dd5ca48
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-03 09:15:53 +0100

    tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,23 +5,26 @@ const Hero = () => {
   return (
     <div>
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <p className="text-[#00DF9a] p-2 font-bold">
+        <p className="text-[#00DF9a] p-2 font-bold ">
           GROWING WITH DATA ANALYTICS
         </p>
-        <h1 className="md:text-7xl sm:text-6xl text-xl font-bold py-6">
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold py-6">
           Grow with data.
         </h1>
         <div>
           <p className="md:text-5xl sm:text-4xl text-xl font-bold">
-            Flex, Flexible financing for
+            Flex, Flexible financing for{" "}
+            <Typed
+              strings={["BTB", "BTC", "SASS"]}
+              typeSpeed={100}
+              backSpeed={100}
+            />
           </p>
-          <Typed
-            strings={["BTC", "BTB", "USD"]}
-            typeSpeed={100}
-            backSpeed={50}
-            loop
-          />
         </div>
+        <p>
+          Monitor your data analytics to increase revenue for BTB, BTC & SASS
+          platform
+        </p>
       </div>
     </div>
   );
