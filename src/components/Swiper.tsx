ee7ad74bb4445511810849b73b6187292a0060cb
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-10 08:49:29 +0100

    swipe

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -1,7 +1,29 @@
 // import React from "react";
 
 const Swiper = () => {
-  return <div>Swiper</div>;
+  return (
+    <div>
+      <div className="swiper">
+        {/* <!-- Additional required wrapper --> */}
+        <div className="swiper-wrapper">
+          {/* <!-- Slides --> */}
+          <div className="swiper-slide">Slide 1</div>
+          <div className="swiper-slide">Slide 2</div>
+          <div className="swiper-slide">Slide 3</div>
+          ...
+        </div>
+        {/* <!-- If we need pagination --> */}
+        <div className="swiper-pagination"></div>
+
+        {/* <!-- If we need navigation buttons --> */}
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+
+        {/* <!-- If we need scrollbar --> */}
+        <div className="swiper-scrollbar"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Swiper;
