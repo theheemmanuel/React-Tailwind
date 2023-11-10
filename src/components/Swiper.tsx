83cf2758d6f165941dde3dcb670a0c66765d7247
Emmanuel Oladiran <oladirane@gmail.com>
2023-11-10 14:57:13 +0100

    lolp

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -6,43 +6,7 @@ import Frame3 from "../assets/Frame (5).png";
 const Swiper = () => {
   return (
     <div>
-      <div className="swiper">
-        {/* <!-- Additional required wrapper --> */}
-        <div className="swiper-wrapper">
-          {/* <!-- Slides --> */}
-          <div className="swiper-slide">
-            <img
-              className="w-12 mx-auto mt-[-3rem] bg-white"
-              src={Frame1}
-              alt=""
-            />
-          </div>
-          <div className="swiper-slide">
-            <img
-              className="w-12 mx-auto mt-[-3rem] bg-white"
-              src={Frame2}
-              alt=""
-            />
-          </div>
-          <div className="swiper-slide">
-            <img
-              className="w-12 mx-auto mt-[-3rem] bg-white"
-              src={Frame3}
-              alt=""
-            />
-          </div>
-          ...
-        </div>
-        {/* <!-- If we need pagination --> */}
-        <div className="swiper-pagination"></div>
-
-        {/* <!-- If we need navigation buttons --> */}
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-button-next"></div>
-
-        {/* <!-- If we need scrollbar --> */}
-        {/* <div className="swiper-scrollbar"></div> */}
-      </div>
+      
     </div>
   );
 };
